614c99b2566303263bb89b39d59cc902320d29bb
sakshiwani2404 <121708158+sakshiwani2404@users.noreply.github.com>
2025-04-03 20:38:19 +0530

    Update and rename ProjectEuclear+1.java to ProjectEuclear1.java

--- ProjectEuclear1.java
+++ ProjectEuclear1.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Solution {
+public class ProjectEuclear1 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int t = in.nextInt();
